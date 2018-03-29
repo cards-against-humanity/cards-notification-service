6b7c695bfcf228fb43844dd0eba5a36b0ce50f54
Thomas Volk <tvolk131@gmail.com>
2018-03-28 22:51:11 -0500

    Added auth module for handling jwt

---
+++ src/main/java/server/JWTAuth.kt
@@ -0,0 +1,58 @@
+package server
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.corundumstudio.socketio.SocketIOClient
+import org.apache.commons.codec.binary.Base64
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
+
+class JWTVerifier(private val password: String) {
+    fun validate(token: String): UserAuthData {
+        val token = JWT.require(Algorithm.HMAC256(password)).build().verify(token)
+        if (token.expiresAt !== null && token.expiresAt.before(Date())) {
+            throw Exception("Token has expired")
+        }
+        val oAuthIdClaim = token.claims["oAuthId"]
+        val oAuthProviderClaim = token.claims["oAuthProvider"]
+        if (oAuthIdClaim?.asString() == null || oAuthProviderClaim?.asString() == null) {
+            throw Exception("Token is missing required user claims")
+        }
+        return JWTUserAuthData(oAuthIdClaim.asString(), oAuthProviderClaim.asString())
+    }
+}
+
+private data class JWTUserAuthData(override val oAuthId: String, override val oAuthProvider: String) : UserAuthData
+
+interface UserAuthData {
+    val oAuthId: String
+    val oAuthProvider: String
+}
+
+
+private fun parseCookies(cookies: String): Map<String, List<String>> {
+    val namedCookieList = cookies.split(";").map { s -> s.trim() }
+
+    val cookieMap: MutableMap<String, MutableList<String>> = HashMap()
+
+    namedCookieList.forEach { cookie ->
+        run {
+            val name = cookie.split("=")[0]
+            val cookieData = cookie.split("$name=")[1]
+            if (cookieMap[name] == null) {
+                cookieMap[name] = ArrayList()
+            }
+            cookieMap[name]!!.add(cookieData)
+        }
+    }
+
+    return cookieMap
+}
+
+fun SocketIOClient.verifyUser(jwtVerifier: JWTVerifier): UserAuthData {
+    val cookiesString = this.handshakeData.httpHeaders["Cookie"]!!
+    val cookies = parseCookies(cookiesString)
+    var token = String(Base64.decodeBase64(cookies["session"]!![0]))
+    return jwtVerifier.validate(token)
+}
---
+++ src/test/java/JWTAuthTest.kt
@@ -0,0 +1,93 @@
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+import server.JWTVerifier
+import java.util.*
+import kotlin.test.assertEquals
+
+class UserCollectionTest {
+    private val password = "1234567890password"
+    private val verifier = JWTVerifier(password)
+
+    @Test
+    fun successfullyDecodeValidTokenWithExpiration() {
+        val oAuthId = "1234"
+        val oAuthProvider = "google"
+
+        val token = JWT.create()
+                .withClaim("oAuthId", oAuthId)
+                .withClaim("oAuthProvider", oAuthProvider)
+                .withExpiresAt(Date(System.currentTimeMillis() + 1000))
+                .sign(Algorithm.HMAC256(password))
+        val deserializedUserData = verifier.validate(token)
+
+        assertEquals(oAuthId, deserializedUserData.oAuthId)
+        assertEquals(oAuthProvider, deserializedUserData.oAuthProvider)
+    }
+
+    @Test
+    fun successfullyDecodeValidTokenNoExpiration() {
+        val oAuthId = "1234"
+        val oAuthProvider = "google"
+
+        val token = JWT.create()
+                .withClaim("oAuthId", oAuthId)
+                .withClaim("oAuthProvider", oAuthProvider)
+                .sign(Algorithm.HMAC256(password))
+        val deserializedUserData = verifier.validate(token)
+
+        assertEquals(oAuthId, deserializedUserData.oAuthId)
+        assertEquals(oAuthProvider, deserializedUserData.oAuthProvider)
+    }
+
+    @Test
+    fun throwErrorForOutdatedToken() {
+        val oAuthId = "1234"
+        val oAuthProvider = "google"
+
+        val token = JWT.create()
+                .withClaim("oAuthId", oAuthId)
+                .withClaim("oAuthProvider", oAuthProvider)
+                .withExpiresAt(Date(System.currentTimeMillis() - 1000))
+                .sign(Algorithm.HMAC256(password))
+        val e = assertThrows(Exception::class.java, { verifier.validate(token) })
+        assert(e.message!!.contains("Token has expired"))
+    }
+
+    @Test
+    fun throwErrorWithInvalidPassword() {
+        val oAuthId = "1234"
+        val oAuthProvider = "google"
+
+        val token = JWT.create()
+                .withClaim("oAuthId", oAuthId)
+                .withClaim("oAuthProvider", oAuthProvider)
+                .sign(Algorithm.HMAC256(password + "1234"))
+
+        val e = assertThrows(Exception::class.java, { verifier.validate(token) })
+        assert(e.message!!.contains("The Token's Signature resulted invalid when verified using the Algorithm"))
+    }
+
+    @Test
+    fun throwErrorWhenMissingUserData() {
+        val oAuthId = "1234"
+
+        val token = JWT.create()
+                .sign(Algorithm.HMAC256(password))
+        val e = assertThrows(Exception::class.java, { verifier.validate(token) })
+        assertEquals("Token is missing required user claims", e.message)
+    }
+
+    @Test
+    fun throwErrorWhenUserDataIsNotStrings() {
+        val oAuthId = "1234"
+
+        val token = JWT.create()
+                .withClaim("oAuthId", 1)
+                .withClaim("oAuthProvider", 1)
+                .sign(Algorithm.HMAC256(password))
+        val e = assertThrows(Exception::class.java, { verifier.validate(token) })
+        assertEquals("Token is missing required user claims", e.message)
+    }
+}
