45bef6cde7a992e0fce2f52a557338b286381dd9
Thomas Volk <tvolk131@gmail.com>
2018-03-30 00:48:08 -0500

    Moved api user fetcher to separate file from interface

---
+++ src/main/java/server/ApiUserFetcher.kt
@@ -0,0 +1,30 @@
+package server
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import server.auth.UserAuthData
+
+class ApiUserFetcher(private val apiHost: String, private val apiPort: Int) : UserFetcher {
+    override fun getById(userId: String): UserAuthData {
+        val request = Request.Builder()
+                .url("http://$apiHost:$apiPort/user/$userId")
+                .build()
+        val response = OkHttpClient().newCall(request).execute()
+        if (response.code() == 404) {
+            throw Exception("User does not exist")
+        }
+        if (!response.isSuccessful) {
+            throw Exception("An error occured fetching user from the api")
+        }
+        val jsonRes = response.body()!!.string()
+        val resMap = ObjectMapper().readValue(jsonRes, Map::class.java)
+
+        val oAuthId = resMap["oAuthId"]!! as String
+        val oAuthProvider = resMap["oAuthProvider"]!! as String
+
+        return APIUserAuthData(oAuthId, oAuthProvider)
+    }
+}
+
+private data class APIUserAuthData(override val oAuthId: String, override val oAuthProvider: String) : UserAuthData
