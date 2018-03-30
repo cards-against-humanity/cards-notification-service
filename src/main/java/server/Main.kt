aeea2ad2cb011fdec9a815c44a6e42058212aa01
Thomas Volk <tvolk131@gmail.com>
2018-03-29 23:59:47 -0500

    Fixed package naming issues

--- src/main/java/server/Main.kt
+++ src/main/java/server/Main.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
-import route.user.EventController
+import route.event.EventController
 
 
 @EnableAutoConfiguration(exclude = [MongoAutoConfiguration::class])
